af677e83bdcb9a90e873baf1f05cf9ffa2ad3bfa
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-04-19 22:48:06 +0700

    chore: fix some styling

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -100,7 +100,7 @@ function App() {
 
   return (
     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="mx-auto max-w-2xl text-center">
+      <div className="mx-auto max-w-2xl">
         {currentPage === 0 && <>
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Address Information</h2>
           <Formik
@@ -212,7 +212,7 @@ function App() {
                   <ErrorMessage name="username" component="div" />
                 </div>
                 <div>
-                  <label htmlFor="password">Password: </label>
+                  <label htmlFor="password" className="block text-sm font-semibold leading-6 text-gray-900">Password: </label>
                   <Field type="password" id="password" name="password" placeholder="Enter your password"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                   <ErrorMessage name="password" component="div" />
