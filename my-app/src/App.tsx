9f60b9fa3ccf67ccb2b1de2495c68645ee27ff14
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-04-19 22:50:55 +0700

    chore: fix some text

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -207,7 +207,7 @@ function App() {
               <Form className="mx-auto mt-16 max-w-xl sm:mt-20">
                 <div className="sm:col-span-2">
                   <label htmlFor="username" className="block text-sm font-semibold leading-6 text-gray-900">Username: </label>
-                  <Field type="text" id="username" name="username" placeholder="Enter your usernamename"
+                  <Field type="text" id="username" name="username" placeholder="Enter your username"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                   <ErrorMessage name="username" component="div" />
                 </div>
