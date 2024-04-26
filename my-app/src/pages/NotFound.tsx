4fd347b4201a69a9e5081b97b48c74f6b1c94941
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-04-26 22:24:09 +0700

    feat: add not found page

---
+++ my-app/src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+    return (
+        <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Page Not Found</h1>
+        </div>
+    );
+}
+
+export default NotFound;
